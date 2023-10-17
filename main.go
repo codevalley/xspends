361718ac3e480dbe4717df1b5c70998a8821dde5
Narayan Babu <codevalley@live.com>
2023-10-17 10:42:14 +0530

    Re-initial commit :-)

---
+++ main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"database/sql"
+	"log"
+
+	_ "github.com/go-sql-driver/mysql"
+)
+
+func main() {
+	db, err := sql.Open("mysql", "root:@tcp(127.0.0.1:4000)/test")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+
+	// Test the connection
+	err = db.Ping()
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Println("Connected to TiDB!")
+}
