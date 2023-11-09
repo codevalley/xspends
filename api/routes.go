3ab818d2225bcab546fe73913d34199f70bcde1a
Narayan Babu <codevalley@live.com>
2023-11-09 22:08:11 +0530

    Basic swagger intergration (WIP)

--- api/routes.go
+++ api/routes.go
@@ -33,27 +33,77 @@ import (
 	"xspends/middleware"
 
 	"github.com/gin-gonic/gin"
+	"github.com/swaggo/gin-swagger"
+
+	swaggerFiles "github.com/swaggo/files"
 )
 
 // SetupRoutes configures all the routes for the application.
 // It takes a gin Engine, a sql DB, and a kvstore RawKVClientInterface as parameters.
+// @title XSpends API
+// @version 1.0
+// @description This is the API for the XSpends application.
+// @host localhost:8080
+// @BasePath /
 func SetupRoutes(r *gin.Engine, db *sql.DB, kvClient kvstore.RawKVClientInterface) {
 	ab := middleware.SetupAuthBoss(r, db, kvClient)
 
 	// Health check endpoint
 	// This endpoint is used to check the health status of the application.
+	// @Summary Health check
+	// @Description Check the health status of the application
+	// @ID get-health
+	// @Produce  json
+	// @Success 200 {object} HealthResponse
+	// @Router /health [get]
 	r.GET("/health", func(c *gin.Context) {
 		c.JSON(200, gin.H{"status": "UP"})
 	})
 
+	r.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
+
 	// Authentication routes using custom JWT handlers
 	// These routes are used for user registration, login, and token refresh.
+	// @tags Authentication
 	auth := r.Group("/auth")
 	{
+		// @Summary Register a new user
+		// @Description Register a new user with email and password
+		// @ID register-user
+		// @Accept  json
+		// @Produce  json
+		// @Param user body User true "User info for registration"
+		// @Success 200 {object} User
+		// @Router /auth/register [post]
 		auth.POST("/register", handlers.JWTRegisterHandler(ab)) // Register a new user
-		auth.POST("/login", handlers.JWTLoginHandler(ab))       // Login an existing user
-		auth.POST("/refresh", handlers.JWTRefreshHandler(ab))   // Refresh JWT token
-		auth.POST("/logout", handlers.JWTLogoutHandler(ab))     // Logout a user
+
+		// @Summary Login an existing user
+		// @Description Login an existing user with email and password
+		// @ID login-user
+		// @Accept  json
+		// @Produce  json
+		// @Param user body User true "User info for login"
+		// @Success 200 {object} User
+		// @Router /auth/login [post]
+		auth.POST("/login", handlers.JWTLoginHandler(ab)) // Login an existing user
+
+		// @Summary Refresh JWT token
+		// @Description Refresh JWT token for an authenticated user
+		// @ID refresh-token
+		// @Accept  json
+		// @Produce  json
+		// @Param refresh_token body string true "Refresh token"
+		// @Success 200 {object} TokenResponse
+		// @Router /auth/refresh [post]
+		auth.POST("/refresh", handlers.JWTRefreshHandler(ab)) // Refresh JWT token
+		// @Summary Logout a user
+		// @Description Logout a user by invalidating their session
+		// @ID logout-user
+		// @Accept  json
+		// @Produce  json
+		// @Success 200 {object} LogoutResponse
+		// @Router /auth/logout [post]
+		auth.POST("/logout", handlers.JWTLogoutHandler(ab)) // Logout a user
 	}
 
 	// All other routes should be protected by the AuthMiddleware
@@ -65,48 +115,250 @@ func SetupRoutes(r *gin.Engine, db *sql.DB, kvClient kvstore.RawKVClientInterfac
 	// These routes are used for managing sources.
 	sources := apiRoutes.Group("/sources")
 	{
-		sources.GET("", handlers.ListSources)         // List all sources
-		sources.POST("", handlers.CreateSource)       // Create a new source
-		sources.GET("/:id", handlers.GetSource)       // Get a specific source
-		sources.PUT("/:id", handlers.UpdateSource)    // Update a specific source
-		sources.DELETE("/:id", handlers.DeleteSource) // Delete a specific source
+		// @Summary List all sources
+		// @Description Get a list of all sources
+		// @ID list-sources
+		// @Accept  json
+		// @Produce  json
+		// @Success 200 {array} Source
+		// @Router /sources [get]
+		sources.GET("", handlers.ListSources)
+
+		// @Summary Create a new source
+		// @Description Create a new source with the provided information
+		// @ID create-source
+		// @Accept  json
+		// @Produce  json
+		// @Param source body Source true "Source info for creation"
+		// @Success 200 {object} Source
+		// @Router /sources [post]
+		sources.POST("", handlers.CreateSource)
+
+		// @Summary Get a specific source
+		// @Description Get a specific source by its ID
+		// @ID get-source
+		// @Accept  json
+		// @Produce  json
+		// @Param id path int true "Source ID"
+		// @Success 200 {object} Source
+		// @Router /sources/{id} [get]
+		sources.GET("/:id", handlers.GetSource)
+
+		// @Summary Update a specific source
+		// @Description Update a specific source by its ID
+		// @ID update-source
+		// @Accept  json
+		// @Produce  json
+		// @Param id path int true "Source ID"
+		// @Param source body Source true "Source info for update"
+		// @Success 200 {object} Source
+		// @Router /sources/{id} [put]
+		sources.PUT("/:id", handlers.UpdateSource)
+
+		// @Summary Delete a specific source
+		// @Description Delete a specific source by its ID
+		// @ID delete-source
+		// @Accept  json
+		// @Produce  json
+		// @Param id path int true "Source ID"
+		// @Success 200 {object} DeleteResponse
+		// @Router /sources/{id} [delete]
+		sources.DELETE("/:id", handlers.DeleteSource)
 	}
 
 	// Category routes
 	// These routes are used for managing categories.
 	categories := apiRoutes.Group("/categories")
 	{
-		categories.GET("", handlers.ListCategories)        // List all categories
-		categories.POST("", handlers.CreateCategory)       // Create a new category
-		categories.GET("/:id", handlers.GetCategory)       // Get a specific category
-		categories.PUT("/:id", handlers.UpdateCategory)    // Update a specific category
-		categories.DELETE("/:id", handlers.DeleteCategory) // Delete a specific category
+		// @Summary List all categories
+		// @Description Get a list of all categories
+		// @ID list-categories
+		// @Accept  json
+		// @Produce  json
+		// @Success 200 {array} Category
+		// @Router /categories [get]
+		categories.GET("", handlers.ListCategories)
+		// @Summary Create a new category
+		// @Description Create a new category with the provided information
+		// @ID create-category
+		// @Accept  json
+		// @Produce  json
+		// @Param category body Category true "Category info for creation"
+		// @Success 200 {object} Category
+		// @Router /categories [post]
+		categories.POST("", handlers.CreateCategory)
+
+		// @Summary Get a specific category
+		// @Description Get a specific category by its ID
+		// @ID get-category
+		// @Accept  json
+		// @Produce  json
+		// @Param id path int true "Category ID"
+		// @Success 200 {object} Category
+		// @Router /categories/{id} [get]
+		categories.GET("/:id", handlers.GetCategory)
+
+		// @Summary Update a specific category
+		// @Description Update a specific category by its ID
+		// @ID update-category
+		// @Accept  json
+		// @Produce  json
+		// @Param id path int true "Category ID"
+		// @Param category body Category true "Category info for update"
+		// @Success 200 {object} Category
+		// @Router /categories/{id} [put]
+		categories.PUT("/:id", handlers.UpdateCategory)
+
+		// @Summary Delete a specific category
+		// @Description Delete a specific category by its ID
+		// @ID delete-category
+		// @Accept  json
+		// @Produce  json
+		// @Param id path int true "Category ID"
+		// @Success 200 {object} DeleteResponse
+		// @Router /categories/{id} [delete]
+		categories.DELETE("/:id", handlers.DeleteCategory)
 	}
 
 	// Tag routes
 	// These routes are used for managing tags.
 	tags := apiRoutes.Group("/tags")
 	{
-		tags.GET("", handlers.ListTags)         // List all tags
-		tags.POST("", handlers.CreateTag)       // Create a new tag
-		tags.GET("/:id", handlers.GetTag)       // Get a specific tag
-		tags.PUT("/:id", handlers.UpdateTag)    // Update a specific tag
-		tags.DELETE("/:id", handlers.DeleteTag) // Delete a specific tag
+		// @Summary List all tags
+		// @Description Get a list of all tags
+		// @ID list-tags
+		// @Accept  json
+		// @Produce  json
+		// @Success 200 {array} Tag
+		// @Router /tags [get]
+		tags.GET("", handlers.ListTags)
+
+		// @Summary Create a new tag
+		// @Description Create a new tag with the provided information
+		// @ID create-tag
+		// @Accept  json
+		// @Produce  json
+		// @Param tag body Tag true "Tag info for creation"
+		// @Success 200 {object} Tag
+		// @Router /tags [post]
+		tags.POST("", handlers.CreateTag)
+
+		// @Summary Get a specific tag
+		// @Description Get a specific tag by its ID
+		// @ID get-tag
+		// @Accept  json
+		// @Produce  json
+		// @Param id path int true "Tag ID"
+		// @Success 200 {object} Tag
+		// @Router /tags/{id} [get]
+		tags.GET("/:id", handlers.GetTag)
+		// @Summary Update a specific tag
+		// @Description Update a specific tag by its ID
+		// @ID update-tag
+		// @Accept  json
+		// @Produce  json
+		// @Param id path int true "Tag ID"
+		// @Param tag body Tag true "Tag info for update"
+		// @Success 200 {object} Tag
+		// @Router /tags/{id} [put]
+		tags.PUT("/:id", handlers.UpdateTag)
+
+		// @Summary Delete a specific tag
+		// @Description Delete a specific tag by its ID
+		// @ID delete-tag
+		// @Accept  json
+		// @Produce  json
+		// @Param id path int true "Tag ID"
+		// @Success 200 {object} DeleteResponse
+		// @Router /tags/{id} [delete]
+		tags.DELETE("/:id", handlers.DeleteTag)
 	}
 
 	// Transaction routes
 	// These routes are used for managing transactions and transaction tags.
 	transactions := apiRoutes.Group("/transactions")
 	{
-		transactions.GET("", handlers.ListTransactions)         // List all transactions
-		transactions.POST("", handlers.CreateTransaction)       // Create a new transaction
-		transactions.GET("/:id", handlers.GetTransaction)       // Get a specific transaction
-		transactions.PUT("/:id", handlers.UpdateTransaction)    // Update a specific transaction
-		transactions.DELETE("/:id", handlers.DeleteTransaction) // Delete a specific transaction
+		// @Summary List all transactions
+		// @Description Get a list of all transactions
+		// @ID list-transactions
+		// @Accept  json
+		// @Produce  json
+		// @Success 200 {array} Transaction
+		// @Router /transactions [get]
+		transactions.GET("", handlers.ListTransactions)
+
+		// @Summary Create a new transaction
+		// @Description Create a new transaction with the provided information
+		// @ID create-transaction
+		// @Accept  json
+		// @Produce  json
+		// @Param transaction body Transaction true "Transaction info for creation"
+		// @Success 200 {object} Transaction
+		// @Router /transactions [post]
+		transactions.POST("", handlers.CreateTransaction)
+
+		// @Summary Get a specific transaction
+		// @Description Get a specific transaction by its ID
+		// @ID get-transaction
+		// @Accept  json
+		// @Produce  json
+		// @Param id path int true "Transaction ID"
+		// @Success 200 {object} Transaction
+		// @Router /transactions/{id} [get]
+		transactions.GET("/:id", handlers.GetTransaction)
+
+		// @Summary Update a specific transaction
+		// @Description Update a specific transaction by its ID
+		// @ID update-transaction
+		// @Accept  json
+		// @Produce  json
+		// @Param id path int true "Transaction ID"
+		// @Param transaction body Transaction true "Transaction info for update"
+		// @Success 200 {object} Transaction
+		// @Router /transactions/{id} [put]
+		transactions.PUT("/:id", handlers.UpdateTransaction)
+
+		// @Summary Delete a specific transaction
+		// @Description Delete a specific transaction by its ID
+		// @ID delete-transaction
+		// @Accept  json
+		// @Produce  json
+		// @Param id path int true "Transaction ID"
+		// @Success 200 {object} DeleteResponse
+		// @Router /transactions/{id} [delete]
+		transactions.DELETE("/:id", handlers.DeleteTransaction)
 
 		// Transaction Tags routes
-		transactions.GET("/:id/tags", handlers.ListTransactionTags)                // List all tags for a specific transaction
-		transactions.POST("/:id/tags", handlers.AddTagToTransaction)               // Add a tag to a specific transaction
-		transactions.DELETE("/:id/tags/:tagID", handlers.RemoveTagFromTransaction) // Remove a tag from a specific transaction
+		// @Summary List all tags for a specific transaction
+		// @Description Get a list of all tags for a specific transaction
+		// @ID list-transaction-tags
+		// @Accept  json
+		// @Produce  json
+		// @Param id path int true "Transaction ID"
+		// @Success 200 {array} Tag
+		// @Router /transactions/{id}/tags [get]
+		transactions.GET("/:id/tags", handlers.ListTransactionTags)
+
+		// @Summary Add a tag to a specific transaction
+		// @Description Add a tag to a specific transaction
+		// @ID add-tag-to-transaction
+		// @Accept  json
+		// @Produce  json
+		// @Param id path int true "Transaction ID"
+		// @Param tag body Tag true "Tag info to add"
+		// @Success 200 {object} Tag
+		// @Router /transactions/{id}/tags [post]
+		transactions.POST("/:id/tags", handlers.AddTagToTransaction)
+
+		// @Summary Remove a tag from a specific transaction
+		// @Description Remove a tag from a specific transaction
+		// @ID remove-tag-from-transaction
+		// @Accept  json
+		// @Produce  json
+		// @Param id path int true "Transaction ID"
+		// @Param tagID path int true "Tag ID"
+		// @Success 200 {object} DeleteResponse
+		// @Router /transactions/{id}/tags/{tagID} [delete]
+		transactions.DELETE("/:id/tags/:tagID", handlers.RemoveTagFromTransaction)
 	}
 }
--- go.mod
+++ go.mod
@@ -9,24 +9,34 @@ require (
 	github.com/sony/sonyflake v1.2.0
 	github.com/tikv/client-go/v2 v2.0.7
 	github.com/volatiletech/authboss/v3 v3.3.0
-	golang.org/x/crypto v0.14.0
+	golang.org/x/crypto v0.15.0
 )
 
 require (
+	github.com/KyleBanks/depth v1.2.1 // indirect
+	github.com/PuerkitoBio/purell v1.2.1 // indirect
+	github.com/PuerkitoBio/urlesc v0.0.0-20170810143723-de5bf2ad4578 // indirect
 	github.com/benbjohnson/clock v1.3.0 // indirect
 	github.com/beorn7/perks v1.0.1 // indirect
 	github.com/cespare/xxhash/v2 v2.2.0 // indirect
 	github.com/coreos/go-semver v0.3.0 // indirect
 	github.com/coreos/go-systemd/v22 v22.3.2 // indirect
+	github.com/cpuguy83/go-md2man/v2 v2.0.3 // indirect
 	github.com/cznic/mathutil v0.0.0-20181122101859-297441e03548 // indirect
 	github.com/dgryski/go-farm v0.0.0-20190423205320-6a90982ecee2 // indirect
 	github.com/elastic/gosigar v0.14.2 // indirect
 	github.com/friendsofgo/errors v0.9.2 // indirect
+	github.com/go-openapi/jsonpointer v0.20.0 // indirect
+	github.com/go-openapi/jsonreference v0.20.2 // indirect
+	github.com/go-openapi/spec v0.20.9 // indirect
+	github.com/go-openapi/swag v0.22.4 // indirect
 	github.com/gogo/protobuf v1.3.2 // indirect
 	github.com/golang/protobuf v1.5.3 // indirect
 	github.com/google/btree v1.1.2 // indirect
 	github.com/google/uuid v1.3.0 // indirect
 	github.com/grpc-ecosystem/go-grpc-middleware v1.1.0 // indirect
+	github.com/josharian/intern v1.0.0 // indirect
+	github.com/mailru/easyjson v0.7.7 // indirect
 	github.com/matttproud/golang_protobuf_extensions v1.0.4 // indirect
 	github.com/opentracing/opentracing-go v1.2.0 // indirect
 	github.com/pingcap/errors v0.11.5-0.20211224045212-9687c2b0f87c // indirect
@@ -38,9 +48,16 @@ require (
 	github.com/prometheus/common v0.39.0 // indirect
 	github.com/prometheus/procfs v0.9.0 // indirect
 	github.com/remyoudompheng/bigfft v0.0.0-20200410134404-eec4a21b6bb0 // indirect
+	github.com/russross/blackfriday/v2 v2.1.0 // indirect
+	github.com/shurcooL/sanitized_anchor_name v1.0.0 // indirect
+	github.com/swaggo/files v1.0.1 // indirect
+	github.com/swaggo/gin-swagger v1.6.0 // indirect
+	github.com/swaggo/swag v1.16.2 // indirect
 	github.com/tiancaiamao/gp v0.0.0-20221230034425-4025bc8a4d4a // indirect
 	github.com/tikv/pd/client v0.0.0-20230329114254-1948c247c2b1 // indirect
 	github.com/twmb/murmur3 v1.1.3 // indirect
+	github.com/urfave/cli/v2 v2.25.7 // indirect
+	github.com/xrash/smetrics v0.0.0-20201216005158-039620a65673 // indirect
 	go.etcd.io/etcd/api/v3 v3.5.2 // indirect
 	go.etcd.io/etcd/client/pkg/v3 v3.5.2 // indirect
 	go.etcd.io/etcd/client/v3 v3.5.2 // indirect
@@ -48,12 +65,15 @@ require (
 	go.uber.org/multierr v1.9.0 // indirect
 	go.uber.org/zap v1.24.0 // indirect
 	golang.org/x/oauth2 v0.6.0 // indirect
-	golang.org/x/sync v0.1.0 // indirect
+	golang.org/x/sync v0.5.0 // indirect
+	golang.org/x/tools v0.15.0 // indirect
 	golang.org/x/xerrors v0.0.0-20220907171357-04be3eba64a2 // indirect
 	google.golang.org/appengine v1.6.7 // indirect
 	google.golang.org/genproto v0.0.0-20230331144136-dcfb400f0633 // indirect
 	google.golang.org/grpc v1.54.0 // indirect
 	gopkg.in/natefinch/lumberjack.v2 v2.2.1 // indirect
+	gopkg.in/yaml.v2 v2.4.0 // indirect
+	sigs.k8s.io/yaml v1.4.0 // indirect
 )
 
 require (
@@ -83,9 +103,9 @@ require (
 	github.com/twitchyliquid64/golang-asm v0.15.1 // indirect
 	github.com/ugorji/go/codec v1.2.11 // indirect
 	golang.org/x/arch v0.5.0 // indirect
-	golang.org/x/net v0.17.0 // indirect
-	golang.org/x/sys v0.13.0 // indirect
-	golang.org/x/text v0.13.0 // indirect
+	golang.org/x/net v0.18.0 // indirect
+	golang.org/x/sys v0.14.0 // indirect
+	golang.org/x/text v0.14.0 // indirect
 	google.golang.org/protobuf v1.31.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
