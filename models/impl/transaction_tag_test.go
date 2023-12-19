063f01ccf5a7a80cce080ac54e2281519161a643
Narayan Babu <codevalley@live.com>
2023-12-20 00:02:23 +0530

    Added test for transaction_tag

--- models/impl/transaction_tag_test.go
+++ models/impl/transaction_tag_test.go
@@ -4,6 +4,7 @@ import (
 	"database/sql"
 	"testing"
 
+	"github.com/DATA-DOG/go-sqlmock"
 	"github.com/golang/mock/gomock"
 	"github.com/pkg/errors"
 	"github.com/stretchr/testify/assert"
@@ -58,4 +59,45 @@ func TestDeleteTransactionTag(t *testing.T) {
 
 	err = mockModelService.TransactionTagModel.DeleteTransactionTag(ctx, transactionID, tagID)
 	assert.Error(t, err)
+}
+
+func TestGetTagsByTransactionID(t *testing.T) {
+	tearDown := setUp(t)
+	defer tearDown()
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	// Create a sqlmock database connection
+	db, mock, err := sqlmock.New()
+	if err != nil {
+		t.Fatalf("An error occurred when creating a mock database connection: %v", err)
+	}
+	defer db.Close()
+
+	// Replace your mockExecutor with db
+	mockModelService.DBService.Executor = db
+
+	transactionID := int64(1)
+
+	// Set up expectations for QueryContext to return mock rows
+	mockRows := sqlmock.NewRows([]string{"id", "name"}).
+		AddRow(1, "Tag1").
+		AddRow(2, "Tag2")
+
+	// Update the expected SQL query pattern to match the actual query
+	expectedSQLPattern := `SELECT t\.id, t\.name FROM tags t JOIN transaction_tags tt ON t\.id = tt\.tag_id WHERE tt\.transaction_id = \?`
+
+	mock.ExpectQuery(expectedSQLPattern).WithArgs(transactionID).
+		WillReturnRows(mockRows)
+
+	tags, err := mockModelService.TransactionTagModel.GetTagsByTransactionID(ctx, transactionID)
+	assert.NoError(t, err)
+	assert.NotNil(t, tags)
+
+	// Add assertions to validate the returned tags
+
+	// Ensure all expectations were met
+	if err := mock.ExpectationsWereMet(); err != nil {
+		t.Errorf("expectations were not met: %s", err)
+	}
 }
