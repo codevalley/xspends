bf7dc1a4ca7f553b42ac4c6487c21b89ea4878c5
Narayan Babu <codevalley@live.com>
2023-12-23 02:24:00 +0530

    Refactor txn test

--- models/impl/transaction_test.go
+++ models/impl/transaction_test.go
@@ -1,6 +1,8 @@
 package impl
 
 import (
+	"context"
+	"database/sql"
 	"testing"
 	"xspends/models/interfaces"
 	xmock "xspends/models/mock"
@@ -10,6 +12,32 @@ import (
 	"github.com/stretchr/testify/mock"
 )
 
+func setupNewMock(t *testing.T) (*sql.DB, sqlmock.Sqlmock) {
+	db, mock, err := sqlmock.New()
+	if err != nil {
+		t.Fatalf("An error occurred when creating a mock database connection: %v", err)
+	}
+	t.Cleanup(func() {
+		db.Close() // ensure the db connection is closed after the test
+	})
+	return db, mock
+}
+func setupForeignKeyMocks(mockM sqlmock.Sqlmock, txn interfaces.Transaction) {
+	// Mock the user existence check
+	mockM.ExpectQuery("^SELECT (.+) FROM users WHERE").
+		WithArgs(txn.UserID).
+		WillReturnRows(sqlmock.NewRows([]string{"1"}).AddRow("1")) // indicating user exists
+
+	// Mock the source existence check
+	mockM.ExpectQuery(`SELECT 1 FROM sources WHERE id = \? AND user_id = \? LIMIT 1`).
+		WithArgs(txn.SourceID, txn.UserID).
+		WillReturnRows(sqlmock.NewRows([]string{"1"}).AddRow("1")) // indicating source exists
+
+	// Mock the category existence check
+	mockM.ExpectQuery("^SELECT (.+) FROM categories WHERE").
+		WithArgs(txn.CategoryID, txn.UserID).
+		WillReturnRows(sqlmock.NewRows([]string{"1"}).AddRow("1")) // indicating category exists
+}
 func TestInsertTransaction(t *testing.T) {
 	tearDown := setUp(t)
 	defer tearDown()
@@ -91,4 +119,62 @@ func TestInsertTransaction(t *testing.T) {
 	if err := mockM.ExpectationsWereMet(); err != nil {
 		t.Errorf("expectations were not met: %s", err)
 	}
+}
+
+func TestInsertTransactionV2(t *testing.T) {
+	tearDown := setUp(t)
+	defer tearDown()
+
+	txn := interfaces.Transaction{
+		UserID:      1,
+		SourceID:    1,
+		CategoryID:  1,
+		Amount:      100.0,
+		Type:        "expense",
+		Description: "Groceries",
+		Tags:        []string{"groceries", "food"},
+	}
+
+	db, mockM := setupNewMock(t)
+
+	defer db.Close()
+
+	mockModelService.DBService.Executor = db
+	mockTagModel := new(xmock.MockTagModel)
+	mockTransactionTagModel := new(xmock.MockTransactionTagModel)
+	mockModelService.TransactionTagModel = mockTransactionTagModel
+	mockModelService.TagModel = mockTagModel
+
+	t.Run("Successful Insertion", func(t *testing.T) {
+		setupForeignKeyMocks(mockM, txn)
+		mockM.ExpectExec("INSERT INTO transactions").
+			WithArgs(sqlmock.AnyArg(), txn.UserID, txn.SourceID, txn.CategoryID, sqlmock.AnyArg(), txn.Amount, txn.Type, txn.Description).
+			WillReturnResult(sqlmock.NewResult(1, 1))
+
+		// Setup for tag handling
+		for _, tag := range txn.Tags {
+			mockTagModel.On(
+				"GetTagByName",
+				mock.Anything, tag, txn.UserID, mock.Anything,
+			).Return(&interfaces.Tag{ID: 1, Name: tag, UserID: txn.UserID}, nil).Once()
+
+			mockTagModel.On(
+				"InsertTag",
+				mock.Anything, mock.AnythingOfType("*interfaces.Tag"), mock.Anything,
+			).Return(nil).Maybe()
+		}
+
+		mockTransactionTagModel.On(
+			"AddTagsToTransaction",
+			mock.Anything, mock.Anything, mock.Anything, txn.UserID, mock.Anything,
+		).Return(nil).Once()
+
+		// Call the method under test
+		err := mockModelService.TransactionModel.InsertTransaction(context.Background(), txn)
+		assert.NoError(t, err)
+		assert.NoError(t, mockM.ExpectationsWereMet())
+		mockTagModel.AssertExpectations(t)
+		mockTransactionTagModel.AssertExpectations(t)
+	})
+
 }
