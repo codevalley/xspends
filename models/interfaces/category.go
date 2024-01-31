a788b8cebc7b9d1ce2e2588d90f56cc5d3e268bb
Narayan Babu <codevalley@live.com>
2024-01-31 21:48:41 +0530

    Added interface methods for category

--- models/interfaces/category.go
+++ models/interfaces/category.go
@@ -20,9 +20,17 @@ type Category struct {
 type CategoryService interface {
 	InsertCategory(ctx context.Context, category *Category, otx ...*sql.Tx) error
 	UpdateCategory(ctx context.Context, category *Category, otx ...*sql.Tx) error
+
+	//deprecated methods
 	DeleteCategory(ctx context.Context, categoryID int64, userID int64, otx ...*sql.Tx) error
 	GetAllCategories(ctx context.Context, userID int64, otx ...*sql.Tx) ([]Category, error)
 	GetCategoryByID(ctx context.Context, categoryID int64, userID int64, otx ...*sql.Tx) (*Category, error)
 	GetPagedCategories(ctx context.Context, page int, itemsPerPage int, userID int64, otx ...*sql.Tx) ([]Category, error)
 	CategoryIDExists(ctx context.Context, categoryID int64, userID int64, otx ...*sql.Tx) (bool, error)
+	//deprected methods end
+	DeleteCategoryNew(ctx context.Context, categoryID int64, scopes []int64, otx ...*sql.Tx) error
+	GetAllScopedCategories(ctx context.Context, scopes []int64, otx ...*sql.Tx) ([]Category, error)
+	GetCategoryByIDNew(ctx context.Context, categoryID int64, scopes []int64, otx ...*sql.Tx) (*Category, error)
+	GetScopedCategories(ctx context.Context, page int, itemsPerPage int, scopes []int64, otx ...*sql.Tx) ([]Category, error)
+	CategoryIDExistsNew(ctx context.Context, categoryID int64, scopes []int64, otx ...*sql.Tx) (bool, error)
 }
