e8729a0a9efc1c60d1e94d63bf4dc150e3840720
Narayan Babu <codevalley@live.com>
2024-01-31 21:53:32 +0530

    Add missing mock methods for source

--- models/mock/source_mock.go
+++ models/mock/source_mock.go
@@ -46,6 +46,31 @@ func (m *MockSourceModel) SourceIDExists(ctx context.Context, sourceID int64, us
 	return args.Bool(0), args.Error(1)
 }
 
+func (m *MockSourceModel) DeleteSourceNew(ctx context.Context, sourceID int64, scopes []int64, otx ...*sql.Tx) error {
+	args := m.Called(ctx, sourceID, scopes, otx)
+	return args.Error(0)
+}
+
+func (m *MockSourceModel) GetSourceByIDNew(ctx context.Context, sourceID int64, scopes []int64, otx ...*sql.Tx) (*interfaces.Source, error) {
+	args := m.Called(ctx, sourceID, scopes, otx)
+	return args.Get(0).(*interfaces.Source), args.Error(1)
+}
+
+func (m *MockSourceModel) GetScopedSources(ctx context.Context, page int, itemsPerPage int, scopes []int64, otx ...*sql.Tx) ([]interfaces.Source, error) {
+	args := m.Called(ctx, page, itemsPerPage, scopes, otx)
+	return args.Get(0).([]interfaces.Source), args.Error(1)
+}
+
+func (m *MockSourceModel) GetSourcesNew(ctx context.Context, scopes []int64, otx ...*sql.Tx) ([]interfaces.Source, error) {
+	args := m.Called(ctx, scopes, otx)
+	return args.Get(0).([]interfaces.Source), args.Error(1)
+}
+
+func (m *MockSourceModel) SourceIDExistsNew(ctx context.Context, sourceID int64, scopes []int64, otx ...*sql.Tx) (bool, error) {
+	args := m.Called(ctx, sourceID, scopes, otx)
+	return args.Bool(0), args.Error(1)
+}
+
 // Idiomatic interface compliance check.
 // Ensure SourceModel implements SourceService
 var _ interfaces.SourceService = &MockSourceModel{}
