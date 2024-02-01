6f7270f4b789eb928fe01e91c08ce1fe943cfaf7
Narayan Babu <codevalley@live.com>
2024-02-01 18:40:51 +0530

    Added mock implementations for tag

--- models/mock/tag_mock.go
+++ models/mock/tag_mock.go
@@ -49,6 +49,30 @@ func (m *MockTagModel) GetTagByName(ctx context.Context, name string, userID int
 	return args.Get(0).(*interfaces.Tag), args.Error(1)
 }
 
+// Mock implementation of DeleteTagNew
+func (m *MockTagModel) DeleteTagNew(ctx context.Context, tagID int64, scopes []int64, otx ...*sql.Tx) error {
+	args := m.Called(ctx, tagID, scopes, otx)
+	return args.Error(0)
+}
+
+// Mock implementation of GetTagByIDNew
+func (m *MockTagModel) GetTagByIDNew(ctx context.Context, tagID int64, scopes []int64, otx ...*sql.Tx) (*interfaces.Tag, error) {
+	args := m.Called(ctx, tagID, scopes, otx)
+	return args.Get(0).(*interfaces.Tag), args.Error(1)
+}
+
+// Mock implementation of GetScopedTags
+func (m *MockTagModel) GetScopedTags(ctx context.Context, scopes []int64, pagination interfaces.PaginationParams, otx ...*sql.Tx) ([]interfaces.Tag, error) {
+	args := m.Called(ctx, scopes, pagination, otx)
+	return args.Get(0).([]interfaces.Tag), args.Error(1)
+}
+
+// Mock implementation of GetTagByNameNew
+func (m *MockTagModel) GetTagByNameNew(ctx context.Context, name string, scopes []int64, otx ...*sql.Tx) (*interfaces.Tag, error) {
+	args := m.Called(ctx, name, scopes, otx)
+	return args.Get(0).(*interfaces.Tag), args.Error(1)
+}
+
 // Idiomatic interface compliance check.
 // Ensure TagModel implements TagService
 var _ interfaces.TagService = &MockTagModel{}
